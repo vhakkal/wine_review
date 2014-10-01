0bfb75d0c056f0e4b5ff51b2d6f1abc4dc3d5cfc
Vikram Hakkal <vhakkal@gmail.com>
2014-09-30 22:51:15 -0400

    Complete Homework 2

---
+++ app/controllers/wines_controller.rb
@@ -0,0 +1,10 @@
+class WinesController < ApplicationController
+	def index
+		@available_at = Time.now
+		@wines = Wine.all
+	end
+
+	def show 
+		@wine = Wine.find(params[:id])
+	end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Wine.create(name: 'Orin Swift Abstract Red',  year: 2012, winery: 'Orin Swift', country: 'USA', varietal: 'Zinfandel, Mourvédre, Tempranillo, Syrah, Petite Sirah')
+Wine.create(name: 'Beringer Bancroft Ranch Merlot', year: 1996, winery: 'Beringer', country: 'USA', varietal: 'Merlot, Cabernet Franc, Cabernet Sauvignon')
+Wine.create(name: 'Crios Rosé of Malbec', year: 2013, winery:' Crios', country: 'Argentina', varietal: 'Malbec')
